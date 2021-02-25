09c47440d10dcd34233c5eedb4f6aa1fc8b4de5f
Guilherme Zago <contatogzii@gmail.com>
2021-02-25 10:18:59 -0300

    ja tem coisas mas eu tenhos outras coisas pra fazer

---
+++ src/components/ExperienceBar.tsx
@@ -0,0 +1,17 @@
+import styles from '../styles/components/ExperienceBar.module.css'
+
+export function ExperienceBar() {
+    return(
+        <header className={styles.experienceBar}>
+            <span>Experience - 0 XP</span>
+            <div>
+                <div style={{width: '50%'}}/>
+
+                <span className={styles.experienceStatus} style={{ left: '50%'}}>
+                    300 XP
+                </span>
+            </div>
+            <span>Experience - 600 XP</span>
+        </header>
+    )
+}
---
+++ src/pages/_document.tsx
@@ -0,0 +1,20 @@
+import Document, {Html,Head,Main,NextScript} from 'next/document'
+
+export default class MyDocument extends Document {
+    render(){
+        return (
+         <Html>
+             <Head>
+      <link rel="preconnect" href="https://fonts.gstatic.com"/>
+      <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@700&display=swap" rel="stylesheet"/>
+      <meta name="theme-color" content="#000000" />
+      </Head>
+             <body>
+                 <Main />
+                 <NextScript/>
+             </body>
+
+         </Html>
+        )
+    }
+}
